d3f9a4826a2188de38725194078ab69c439d2e7c
Thomas Urban <thomas.urban@cepharum.de>
2017-02-25 11:09:36 +0100

    revising simple test to use onStarted promise instead of fixed delay

--- test/scripts/node/01-least.js
+++ test/scripts/node/01-least.js
@@ -15,18 +15,16 @@ suite( "Least project with node" );
 test( "running server", function runningServer( done ) {
 	let server = HTTP.createServer( hitchy );
 
-	options.onStarted.catch( done );
-
 	server.listen( function() {
 		let socket = server.address();
 
-		setTimeout( function queryingServer() {
+		options.onStarted.then( function() {
 			HTTP.get( "http://127.0.0.1:" + socket.port, function( res ) {
 				assert.equal( res.statusCode, 404, "query did not yield 404 response status" );
 
 				hitchy();
 				done();
 			} );
-		}, 1000 );
+		}, done );
 	} );
 } );
