29e0b7678b1bcd77a583a4ca3f5d11bd6a39cdc0
Thomas Urban <thomas.urban@cepharum.de>
2017-02-26 11:41:31 +0100

    adding another load simulation using random queries

--- test/scripts/basics/load.js
+++ test/scripts/basics/load.js
@@ -17,8 +17,13 @@ suite( "Serving core-only project load simulation (250k requests split into 500 
 
 	this.timeout( 60000 );
 
+	const Chunks = 500;
+	const RequestsPerChunk = 500;
+	const DelayPerChunk = 10;
+
+
 	test( "misses GETting", function() {
-		let requests = new Array( 500 );
+		let requests = new Array( RequestsPerChunk );
 
 		for ( let i = 0, length = requests.length; i < length; i++ ) {
 			requests[i] = Test.get( "/" )
@@ -29,9 +34,28 @@ suite( "Serving core-only project load simulation (250k requests split into 500 
 				} );
 		}
 
-		return Promises.each( new Array( 500 ), function( value, index ) {
+		return Promises.each( new Array( Chunks ), function( value, index ) {
 			return Promise.all( requests )
-				.then( () => Promises.delay( 10 ) );
+				.then( () => Promises.delay( DelayPerChunk ) );
+		} );
+	} );
+
+	test( "GETs /view/read/<id> w/ random <id>", function() {
+		let requests = new Array( RequestsPerChunk );
+
+		for ( let i = 0, length = requests.length; i < length; i++ ) {
+			let value = String( 1000 * Math.random() );
+			requests[i] = Test.get( "/view/read/" + value )
+				.then( function( response ) {
+					response.should.have.value( "statusCode", 200 );
+					response.should.be.json();
+					response.data.id.should.be.String().and.equal( value );
+				} );
+		}
+
+		return Promises.each( new Array( Chunks ), function( value, index ) {
+			return Promise.all( requests )
+				.then( () => Promises.delay( DelayPerChunk ) );
 		} );
 	} );
 } );
