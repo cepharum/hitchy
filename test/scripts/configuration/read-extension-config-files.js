c5cceb7980ab53a1b28a0c00f1f4a4959b9bf460
Thomas Urban <thomas.urban@cepharum.de>
2019-04-12 11:02:10 +0200

    adding unit test for new feature reading configuration files of extensions

---
+++ test/scripts/configuration/read-extension-config-files.js
@@ -0,0 +1,59 @@
+/**
+ * (c) 2019 cepharum GmbH, Berlin, http://cepharum.de
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 cepharum GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @author: cepharum
+ */
+
+"use strict";
+
+let options = {
+	projectFolder: "test/projects/configuration/extension-file",
+	//debug: true,
+};
+
+const Test = require( "../../../tools" ).test;
+const Hitchy = require( "../../../injector" )[process.env.HITCHY_MODE || "node"];
+
+require( "should" );
+require( "should-http" );
+
+// ----------------------------------------------------------------------------
+
+suite( "Hitchy", function() {
+	const hitchy = Hitchy( options );
+	let server = null;
+
+	suiteSetup( () => Test.startServer( hitchy ).then( s => ( server = s ) ) );
+	suiteTeardown( () => server && server.stop() );
+
+	test( "is reading configuration files provided by discovered extension", function() {
+		return hitchy.onStarted.then( () => {
+			hitchy.hitchy.runtime.config.should.be.Object();
+			hitchy.hitchy.runtime.config.should.have.property( "custom" ).which.is.an.Object().which.has.property( "visible" ).which.is.true();
+			hitchy.hitchy.runtime.config.should.have.property( "customConfig" ).which.is.an.Object().which.has.property( "additional" ).which.is.true();
+			hitchy.hitchy.runtime.config.should.have.property( "customConfig" ).which.is.an.Object().which.has.property( "overloaded" ).which.is.equal( "yes" );
+		} );
+	} );
+} );
