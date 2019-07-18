3812586d89cdaa0a7f323fe49d920900b0f71b06
Thomas Urban <thomas.urban@cepharum.de>
2019-07-19 00:34:27 +0200

    adding tests covering policy-routing requesting different kinds of paths using several methods

---
+++ test/scripts/routing/policies.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let options = {
+	projectFolder: "test/projects/policies",
+	// debug: true,
+};
+
+const Test = require( "../../../tools/index" ).test;
+const Hitchy = require( "../../../injector" ).express;
+
+const { describe, it, before, after } = require( "mocha" );
+require( "should" );
+require( "should-http" );
+
+// ----------------------------------------------------------------------------
+
+describe( "Policy routing", function() {
+	const hitchy = Hitchy( options );
+	let server = null;
+
+	before( () => Test.startServer( hitchy ).then( s => ( server = s ) ) );
+	after( () => server && server.stop() );
+
+	for ( const method of [ "GET", "PUT", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS", "TRACE" ] ) {
+		for ( const path of [ "/prefix/deep/path", "/prefix/deep/", "/prefix/deep", "/prefix/", "/prefix" ] ) {
+			it( `is involved when ${method} ${path}`, function() {
+				return hitchy.onStarted.then( () => Test[method.toLowerCase()]( path )
+					.then( response => {
+						response.should.have.status( 200 );
+						response.headers["x-policy"].should.be.String().and.equal( "responded" );
+					} ) );
+			} );
+		}
+	}
+} );
