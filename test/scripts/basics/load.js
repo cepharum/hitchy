00f5f9c8545b9a39dc23ad91bd5de4c49f486c38
Thomas Urban <thomas.urban@cepharum.de>
2020-03-01 07:52:41 +0100

    fixing severe issues with load-simulation tests

--- test/scripts/basics/load.js
+++ test/scripts/basics/load.js
@@ -5,72 +5,88 @@ const options = {
 	// debug: true,
 };
 
-const { suite, test, suiteTeardown, suiteSetup } = require( "mocha" );
+const { describe, it, after, before } = require( "mocha" );
 
 require( "should" );
 require( "should-http" );
 
 const Test = require( "../../../tools" ).test;
 const Promises = require( "../../../tools" ).promise;
-const Hitchy = require( "../../../injector" ).node;
 
 // ----------------------------------------------------------------------------
 
-suite( "Serving core-only project load simulation (250k requests split into 500 chunks)", function() {
+describe( "Simulating load by sending 5k requests split into chunks of 500 each in short-term succession", function() {
 	this.timeout( 60000 );
 
-	const Chunks = 500;
+	const Chunks = 10;
 	const RequestsPerChunk = 500;
 	const DelayPerChunk = 10;
 
-	const hitchy = Hitchy( options );
-	let server = null;
+	const ctx = {};
 
-	suiteSetup( () => Test.startServer( hitchy ).then( s => ( server = s ) ) );
-	suiteTeardown( () => server && server.stop() );
+	before( Test.before( ctx, options ) );
+	after( Test.after( ctx ) );
 
-	test( "misses GETting /missing", function() {
+	it( "misses GETting /missing", function() {
 		if ( process.env.SKIP_LOAD_TESTS ) {
 			this.skip();
+			return undefined;
 		}
 
-		const requests = new Array( RequestsPerChunk );
+		let count = 0;
 
-		for ( let i = 0, length = requests.length; i < length; i++ ) {
-			requests[i] = Test.get( "/missing" )
-				.then( function( response ) {
-					response.should.have.value( "statusCode", 404 );
-					response.should.be.html();
-					response.text.should.be.String().and.match( /\bnot\s+found\b/i ).and.match( /<html\b/i );
+		const check = response => {
+			response.should.have.value( "statusCode", 404 );
+			response.should.be.html();
+			response.text.should.be.String().and.match( /\bnot\s+found\b/i ).and.match( /<html\b/i );
+			count++;
+		};
+
+		return Promises.each( new Array( Chunks ), ( _, chunkIndex ) => {
+			return Promises.delay( DelayPerChunk * chunkIndex )
+				.then( () => {
+					const requests = new Array( RequestsPerChunk );
+
+					for ( let i = 0, length = requests.length; i < length; i++ ) {
+						requests[i] = ctx.get( "/missing" ).then( check );
+					}
+
+					return Promise.all( requests );
 				} );
-		}
-
-		return hitchy.onStarted.then( () => Promises.each( new Array( Chunks ), () => {
-			return Promise.all( requests )
-				.then( () => Promises.delay( DelayPerChunk ) );
-		} ) );
+		} )
+			.then( () => {
+				count.should.be.equal( Chunks * RequestsPerChunk );
+			} );
 	} );
 
-	test( "GETs /view/read/<id> w/ random <id>", function() {
+	it( "GETs /view/read/<id> w/ random <id>", function() {
 		if ( process.env.SKIP_LOAD_TESTS ) {
 			this.skip();
+			return undefined;
 		}
 
-		const requests = new Array( RequestsPerChunk );
+		let count = 0;
 
-		for ( let i = 0, length = requests.length; i < length; i++ ) {
-			const value = String( Math.ceil( 1000 * Math.random() ) );
-			requests[i] = Test.get( "/view/read/" + value )
-				.then( function( response ) {
-					response.should.have.value( "statusCode", 200 );
-					response.should.be.json();
-					response.data.id.should.be.String().and.equal( value );
+		return Promises.each( new Array( Chunks ), ( _, chunkIndex ) => {
+			return Promises.delay( DelayPerChunk * chunkIndex )
+				.then( () => {
+					const requests = new Array( RequestsPerChunk );
+
+					for ( let i = 0, length = requests.length; i < length; i++ ) {
+						const value = String( Math.ceil( 1000 * Math.random() ) );
+						requests[i] = ctx.get( "/view/read/" + value ).then( response => {
+							response.should.have.value( "statusCode", 200 );
+							response.should.be.json();
+							response.data.id.should.be.String().and.equal( value );
+							count++;
+						} );
+					}
+
+					return Promise.all( requests );
 				} );
-		}
-
-		return hitchy.onStarted.then( () => Promises.each( new Array( Chunks ), () => {
-			return Promise.all( requests )
-				.then( () => Promises.delay( DelayPerChunk ) );
-		} ) );
+		} )
+			.then( () => {
+				count.should.be.equal( Chunks * RequestsPerChunk );
+			} );
 	} );
 } );
